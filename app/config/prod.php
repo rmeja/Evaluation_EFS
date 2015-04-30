56dfbb2f0777d9ce52f5fca0ffce6ec1f310ac57
Rémy MEJA <remy.meja@univ-lorraine.fr>
2015-04-30 10:47:53 +0200

    Guerre des logs terminés

--- app/config/prod.php
+++ app/config/prod.php
@@ -2,8 +2,6 @@
 
 // configure your app for the production environment
 
-use Silex\Provider\MonologServiceProvider;
-
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../../var/cache/twig');
 $app['twig.form.templates'] = array('bootstrap_3_layout.html.twig');
@@ -19,7 +17,3 @@ $app['twig.form.templates'] = array('bootstrap_3_layout.html.twig');
 //   'port' => '3306',
 //   'charset' => 'utf8'
 // );
-
-$app->register(new MonologServiceProvider(), array(
-  'monolog.logfile' => __DIR__.'/../../var/logs/evalefs_prod.log',
-));
